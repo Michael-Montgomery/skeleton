c032e8b13d6ee61a85a784f026cbebc3c5cb990a
michael-montgomery <montgomery.michael@hotmail.com>
2017-08-31 19:35:49 -0400

    skeleton created.

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+var app = angular.module('sample', [
+    'ngRoute',
+    'sample.welcome'
+]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        redirectTo: '/welcome'
+    }).otherwise({
+        redirectTo: '/welcome'
+    });
+
+    $routeProvider.when('/welcome', {
+        templateUrl: 'public/views/templates/welcome.tpl.html',
+        controller: 'welcomeController'
+    });
+
+    $routeProvider.when('/about', {
+        templateUrl: 'public/views/templates/about.tpl.html',
+        controller: 'aboutController'
+    });
+
+    $routeProvider.when('/homes', {
+        templateUrl: 'public/views/templates/homes.tpl.html',
+        controller: 'homesController'
+    });
+
+    $routeProvider.when('/home', {
+        templateUrl: 'public/views/templates/home.tpl.html',
+        controller: 'homeController'
+    });
+
+    $routeProvider.when('/contact', {
+        templateUrl: 'public/views/templates/contact.tpl.html',
+        controller: 'contactController'
+    });
+
+    $routeProvider.when('/admin-login', {
+        templateUrl: 'public/views/templates/admin-login.tpl.html',
+        controller: 'adminLoginController'
+    })
+});
+
+app.controller('mainController', function($scope) {
+
+
+
+})
---
+++ public/js/welcome.js
@@ -0,0 +1,6 @@
+var app = angular.module('sample.welcome', []);
+
+app.controller('welcomeController', function($scope, $location) {
+
+
+});
